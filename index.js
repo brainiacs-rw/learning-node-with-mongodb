fa733036142df817d6663ad16e24e9db1c57808e
izabayo7 <cedricizabayo7@gmail.com>
2024-07-06 17:55:21 +0200

    chore: added parametarized get

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get('/welcome', (req, res) => {
     res.send({ message: "Welcome Mr Cedric" });
 });
 
+// Dynamic route with parameters
+app.get('/welcome/:name', (req, res) => {
+    const name = req.params.name;
+    res.send({ message: `Welcome Mr ${name}` });
+});
+
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
